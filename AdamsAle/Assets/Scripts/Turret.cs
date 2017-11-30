f3925d50c330b48fccc6901db97ea1cfdcb682b3
l3th4l <safalyaglobal@gmail.com>
2017-11-30 19:08:13 +0530

    Turrets can shoot now

--- AdamsAle/Assets/Scripts/Turret.cs
+++ AdamsAle/Assets/Scripts/Turret.cs
@@ -8,7 +8,26 @@ public class Turret : MonoBehaviour
     private Transform target;
     public bool IsHacked;
     public float TurretRadius;
-	void Start ()
+
+    //For shooting
+    float nextTimeToFire = 0.0f;
+    public float fireRate;
+    public Transform weaponTransform;
+    public float range = 100f;
+    public float damage = 10f;
+
+    [SerializeField]
+    public LayerMask mask;
+
+
+    [Space]
+    [Header("Effects")]
+    public float impactForce = 20f;
+    public GameObject hitEffectPrefab;
+    public ParticleSystem muzzleFlash;
+
+
+    void Start ()
     {
         IsHacked = false;
         target = GameObject.FindGameObjectWithTag("Player").transform;
@@ -26,16 +45,76 @@ public class Turret : MonoBehaviour
                 {
                     target = entity.transform;
                     print(entity.tag);
+                    //Breaks the loop after shooting a single entity
                     break;
                 }
             }
         }
         if ((transform.position - target.position).sqrMagnitude < TurretRadius)
         {
+            //Look
             float angle = Mathf.Atan2((transform.position - target.position).y, (transform.position - target.position).x) * Mathf.Rad2Deg;
             Quaternion rot = Quaternion.Euler(new Vector3(0, 0, angle));
             transform.rotation = Quaternion.Lerp(transform.rotation, rot, 0.2f);
+
+            //Shoot
+            if (Time.time >= nextTimeToFire)
+            {
+                nextTimeToFire = Time.time + 1 / fireRate;
+                Shoot();
+            }
         }
         
+    }
+    void Shoot()
+    {
+        //Debug.Log("Shoot" + nextTimeToFire);
+        
+        //Debug.Log("Shooting");
+
+        //We are shooting, calling OnShoot method
+        OnShoot();
+
+        RaycastHit _hit;
+        Debug.Log("Hit created" + target.position);
+
+        Debug.DrawRay(weaponTransform.position, (target.position - transform.position).normalized, Color.red);
+
+        if (Physics.Raycast(weaponTransform.position, (target.position-transform.position).normalized, out _hit, range ))
+        {
+
+            Debug.Log("Hit" + target.position);
+
+            Enemy DMG_target = _hit.transform.GetComponent<Enemy>();
+            if (DMG_target != null)
+            {
+                DMG_target.TakeDamage(damage);
+            }
+
+            if (_hit.rigidbody != null)
+            {
+                _hit.rigidbody.AddForce(-_hit.normal * impactForce);
+            }
+
+            //we hit something, calling onHit method
+            OnHit(_hit.point, _hit.normal);
+        }
+    }
+
+    void OnHit(Vector3 _hitPos, Vector3 _normal)
+    {
+
+        //Hit Effect
+        GameObject hitEffect = (GameObject)Instantiate(hitEffectPrefab, _hitPos, Quaternion.LookRotation(_normal));
+        Destroy(hitEffect, 2f);
+    }
+
+    void OnShoot()
+    {
+        // We are shooting, subtracting ammo
+        //currentAmmo--;
+
+        //Shoot Effect
+        muzzleFlash.Play();
     }
 }
