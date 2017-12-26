f89ac9a8c65f28d5e6d3bf61c2ed626f3b1b1c4c
Supantha Paul <gameover7853@gmail.com>
2017-12-26 14:20:06 +0530

    Fixed cover bug

--- AdamsAle/Assets/Scripts/GameMechanics/CoverTrigger.cs
+++ AdamsAle/Assets/Scripts/GameMechanics/CoverTrigger.cs
@@ -8,19 +8,29 @@ public class CoverTrigger : MonoBehaviour {
 	[Tooltip("The Z coordinate of player when in cover")]
 	public float coverZ;
 	public GameObject player;
-
+	bool isInCover = false;
 
 	private void OnTriggerStay(Collider col) {
 		if(col.CompareTag("Player"))
 		{
 			if(Input.GetKey(KeyCode.C))
 			{
+				isInCover = true;
 				player.transform.DOMoveZ(coverZ, 0.8f);
 			}
 			else
 			{
+				isInCover = false;
 				player.transform.DOMoveZ(0f, 0.8f);
 			}
+		}
+	}
+
+	private void OnTriggerExit(Collider col) 
+	{
+		if(col.CompareTag("Player") && isInCover)
+		{
+			player.transform.DOMoveZ(0f, 0.8f);
 		}
 	}
 }
