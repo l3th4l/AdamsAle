3d4b7682c0d5e927679dcf231245e857b92a5876
l3th4l <safalyaglobal@gmail.com>
2017-12-27 09:25:29 +0530

    Locked FPS to 120

---
+++ AdamsAle/Assets/FPSLock.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSLock : MonoBehaviour
+{
+	void Update ()
+    {
+        Application.targetFrameRate = 120;
+    }
+}
