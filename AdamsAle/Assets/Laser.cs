46d5fd7c5b7533a9b7497af8c16542ec3a2759e8
l3th4l <safalyaglobal@gmail.com>
2018-01-12 15:35:32 +0530

    Added lasers

--- AdamsAle/Assets/Laser.cs
+++ AdamsAle/Assets/Laser.cs
@@ -10,38 +10,51 @@ public class Laser : MonoBehaviour
     private MeshRenderer MRend;
 
     public LayerMask LMask;
+    public LayerMask AlarmMask;
+
+    public float AlarmRad;
 
     LineRenderer LineRend;
+
+    public bool Vertical;
 
     private void Start()
     {
         Trigger = GetComponent<Collider>();
         MRend = GetComponent<MeshRenderer>();
 
-        try
-        {
-            LineRend = GetComponent<LineRenderer>();
-        }
-        catch(System.NullReferenceException)
-        {
-            Debug.Log("No line renderer assigned.");
-            LineRend = new LineRenderer();
-        }
-
         if (LineRend == null)
-            LineRend = new LineRenderer();
-        LineRend.SetPosition(0, transform.position);
-        LineRend.SetWidth(0.5f, 0.5f);
+            LineRend = gameObject.AddComponent<LineRenderer>();
+
+        LineRend.SetVertexCount(2);
+        LineRend.SetWidth(0.25f,0.25f);
+
     }
 
     void Update ()
     {
+        LineRend.SetPosition(0, transform.position);
         if (Time.time%(SwitchTime*2) >= SwitchTime)
         {
             RaycastHit _hit;
-            if(Physics.Raycast(transform.position,transform.up*-1,out _hit,LMask))
+            if(Physics.Raycast(transform.position,-((Vertical/*Checks if movement is set to vertical*/) ? Vector3.right:Vector3.up),out _hit,LMask))
             {
+                print(_hit.point);
                 LineRend.SetPosition(1, _hit.point);
+                if(_hit.transform.CompareTag("Player")) // If laser hits the player
+                {
+                    Collider[] HostileEnt = Physics.OverlapSphere(transform.position, AlarmRad, AlarmMask);// Alarm everyone around the laser
+                    foreach(Collider Ent in HostileEnt)
+                    {
+                        HostileAI H_AI = Ent.GetComponent<HostileAI>();
+                        if(H_AI != null)
+                        {
+                            H_AI.aware = true;
+                            H_AI.distracted = true;
+                            H_AI.distractionPos = _hit.point;
+                        }
+                    }
+                }
             }
         }
         else
