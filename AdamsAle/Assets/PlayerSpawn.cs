e2b114e492199f61137a0713e775f75f6d18116d
l3th4l <safalyaglobal@gmail.com>
2018-01-19 10:42:16 +0530

    Added player spawn script to game manager

---
+++ AdamsAle/Assets/PlayerSpawn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawn : MonoBehaviour
+{
+    public Transform PlayerSpawnPos;
+    public GameObject PlayerPrefab;
+    
+	void Start ()
+    {
+        GameObject.Instantiate(PlayerPrefab, PlayerSpawnPos.position, PlayerPrefab.transform.rotation);		
+	}
+}
