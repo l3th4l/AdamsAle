5d9d5352363c3d53c00afaaba877e1953884b428
Mathias Wagner Nielsen <mathiasvalentinwagner@gmail.net>
2018-05-20 13:45:03 +0200

    Add sequenced dialogue

---
+++ AdamsAle/Assets/Scripts/Dialogue/SequencedDialogueProvider.cs
@@ -0,0 +1,24 @@
+﻿namespace Dialogue
+{
+    using UnityEngine;
+
+    public sealed class SequencedDialogueProvider : DialogueProvider
+    {
+        [SerializeField]
+        private string[] text;
+
+        private int lastIndex = -1;
+
+        public override bool TryGetNext(out string text)
+        {
+            if (this.lastIndex < this.text.Length - 1)
+            {
+                text = this.text[++this.lastIndex];
+                return true;
+            }
+
+            text = string.Empty;
+            return false;
+        }
+    }
+}
